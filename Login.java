d59fc5ae46989986a27c58a4e76d46d84e32149c
louispCx <99268939+louispCx@users.noreply.github.com>
2022-02-16 22:27:01 +0900

    Update Login.java

--- Login.java
+++ Login.java
@@ -22,8 +22,8 @@ class Login
             String email = request.getParameter("email");
             String token = request.getParameter("password");
 
-            //String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
-            String sql = "select * from users where (email ='" + "' and password ='" + token + "')";
+            String sql = "select * from users where (email ='" + email +"' and password ='" + token + "')";
+            //String sql = "select * from users where (email ='" + "' and password ='" + token + "')";
 
             Connection connection = pool.getConnection();
             Statement statement = connection.createStatement();
